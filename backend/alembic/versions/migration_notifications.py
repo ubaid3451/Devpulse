8837bc7c5e55adb8351118f32e797b5699628e86
ubaid3451 <ubaidurrehman3451@gmail.com>
2026-08-20 16:12:28 +0500

    fix

--- backend/alembic/versions/migration_notifications.py
+++ backend/alembic/versions/migration_notifications.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = "notifications_001"
-down_revision: Union[str, None] = "rbac_001"
+down_revision: Union[str, None] = "multi_device_001"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
